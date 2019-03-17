b3533f436f04953fdb7a9227a66ac506d07e16b3
Dan Lydon <dlydon@liveearth.com>
2019-03-17 14:09:41 -0500

    More updates from bob

--- models/configuration.go
+++ models/configuration.go
@@ -1,16 +1,16 @@
 package models
 
-// import (
-// 	"os"
-// 	"fmt"
-// )
+import (
+	"os"
+	"fmt"
+)
 
 func GetCurrentDirectory() string {
-	return "/home/ec2-user/projects/src/github.com/dtlydon/go-iyfd"
-	// workingDir, err := os.Getwd()
-	// if err != nil{
-	// 	fmt.Println("Error attempting to get WD: ", err.Error())
-	// 	return ""
-	// }
-	// return workingDir
+	// return "/home/ec2-user/projects/src/github.com/dtlydon/go-iyfd"
+	workingDir, err := os.Getwd()
+	if err != nil{
+		fmt.Println("Error attempting to get WD: ", err.Error())
+		return ""
+	}
+	return workingDir
 }
